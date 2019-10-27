7816c2fa40b44584cbddfa60cafebd54bf040197
isisisisisitch <dallucus@gmail.com>
2019-10-27 19:38:45 -0400

    modify rbtree

--- src/com/luoExpress/_09_Set/tree/RBTree.java
+++ src/com/luoExpress/_09_Set/tree/RBTree.java
@@ -1,4 +1,4 @@
-package com.luoExpress._08_RBTree.tree;
+package com.luoExpress._09_Set.tree;
 
 import java.util.Comparator;
 
@@ -23,10 +23,10 @@ public class RBTree<E> extends BBST<E> {
 			black(node);
 			return;
 		}
-
+		
 		// 如果父节点是黑色，直接返回
 		if (isBlack(parent)) return;
-
+		
 		// 叔父节点
 		Node<E> uncle = parent.sibling();
 		// 祖父节点
@@ -41,7 +41,6 @@ public class RBTree<E> extends BBST<E> {
 		
 		// 叔父节点不是红色
 		if (parent.isLeftChild()) { // L
-			red(grand);
 			if (node.isLeftChild()) { // LL
 				black(parent);
 			} else { // LR
@@ -50,7 +49,6 @@ public class RBTree<E> extends BBST<E> {
 			}
 			rotateRight(grand);
 		} else { // R
-			red(grand);
 			if (node.isLeftChild()) { // RL
 				black(node);
 				rotateRight(parent);
@@ -212,7 +210,7 @@ public class RBTree<E> extends BBST<E> {
 //					rotateLeft(sibling);
 //					sibling = parent.left;
 //				}
-//
+//				
 //				color(sibling, colorOf(parent));
 //				black(sibling.left);
 //				black(parent);
@@ -221,40 +219,40 @@ public class RBTree<E> extends BBST<E> {
 //		}
 //	}
 
-		private Node<E> color(Node<E> node, boolean color) {
-			if (node == null) return node;
-			((RBNode<E>)node).color = color;
-			return node;
-		}
+	private Node<E> color(Node<E> node, boolean color) {
+		if (node == null) return node;
+		((RBNode<E>)node).color = color;
+		return node;
+	}
+	
+	private Node<E> red(Node<E> node) {
+		return color(node, RED);
+	}
+	
+	private Node<E> black(Node<E> node) {
+		return color(node, BLACK);
+	}
+	
+	private boolean colorOf(Node<E> node) {
+		return node == null ? BLACK : ((RBNode<E>)node).color;
+	}
+	
+	private boolean isBlack(Node<E> node) {
+		return colorOf(node) == BLACK;
+	}
+	
+	private boolean isRed(Node<E> node) {
+		return colorOf(node) == RED;
+	}
+	
+	@Override
+	protected Node<E> createNode(E element, Node<E> parent) {
+		return new RBNode<>(element, parent);
+	}
 
-		private Node<E> red(Node<E> node) {
-			return color(node, RED);
-		}
-
-		private Node<E> black(Node<E> node) {
-			return color(node, BLACK);
-		}
-
-		private boolean colorOf(Node<E> node) {
-			return node == null ? BLACK : ((RBNode<E>)node).color;
-		}
-
-		private boolean isBlack(Node<E> node) {
-			return colorOf(node) == BLACK;
-		}
-
-		private boolean isRed(Node<E> node) {
-			return colorOf(node) == RED;
-		}
-
-		@Override
-		protected Node<E> createNode(E element, Node<E> parent) {
-			return new RBNode<>(element, parent);
-		}
-
-		private static class RBNode<E> extends Node<E> {
-			boolean color = RED;
-			public RBNode(E element, Node<E> parent) {
+	private static class RBNode<E> extends Node<E> {
+		boolean color = RED;
+		public RBNode(E element, Node<E> parent) {
 			super(element, parent);
 		}
 		
